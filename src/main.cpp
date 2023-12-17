f1c89e198953729859cd7a4b62b5c9560967c399
Jesper Melin <jesper.melin87@gmail.com>
2023-12-17 18:45:54 +0100

    Add description to main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 #include "application.h"
 
+/**
+ * @brief Represents the main application.
+ */
 Application app;
+
+/**
+ * @brief The main entry point of the program.
+ *
+ * This function initializes the serial communication, waits for 1 second,
+ * and then initializes the application. It is called once at the start of the program.
+ */
 void setup()
 {
   Serial.begin(9600);
@@ -9,6 +19,12 @@ void setup()
   app.init();
 }
 
+/**
+ * @brief The main loop of the program.
+ *
+ * This function continuously runs the application's logic.
+ * It is called repeatedly after the setup() function.
+ */
 void loop()
 {
   app.run();
