27d0cd13ee193ff5b5fb904da5576642073ef25f
Jesper Melin <jesper.melin87@gmail.com>
2024-01-13 14:55:47 +0100

    Add HUB75 support to Display class
    Add initital methods and variables.

--- lib/display/src/display.cpp
+++ lib/display/src/display.cpp
@@ -2,12 +2,83 @@
 
 Display::Display()
 {
-    // Constructor implementation
+    HUB75_I2S_CFG config(matrixWidth, matrixHeight, matrixChainLength);
+    // Configure green and blue channels
+    config.gpio.b1 = 26;
+    config.gpio.b2 = 12;
+    config.gpio.g1 = 27;
+    config.gpio.g2 = 13;
+    config.clkphase = false;
+
+    dma_display = new MatrixPanel_I2S_DMA(config);
+    dma_display->begin();
+
+    dma_display->clearScreen();
+    dma_display->fillScreen(MatrixPanel_I2S_DMA::color565(0, 0, 0));
+    dma_display->setTextWrap(false);
 }
 
 Display::~Display()
 {
     // Destructor implementation
+}
+
+void Display::clearScreen()
+{
+    dma_display->clearScreen();
+    dma_display->fillScreen(MatrixPanel_I2S_DMA::color565(0, 0, 0));
+}
+
+uint16_t Display::color565(Color color)
+{
+    switch (color)
+    {
+    case BLACK:
+        return MatrixPanel_I2S_DMA::color565(0, 0, 0);
+    case WHITE:
+        return MatrixPanel_I2S_DMA::color565(255, 255, 255);
+    case RED:
+        return MatrixPanel_I2S_DMA::color565(255, 0, 0);
+    case GREEN:
+        return MatrixPanel_I2S_DMA::color565(0, 255, 0);
+    case BLUE:
+        return MatrixPanel_I2S_DMA::color565(0, 0, 255);
+    case YELLOW:
+        return MatrixPanel_I2S_DMA::color565(255, 255, 0);
+    case DARK_ORANGE:
+        return MatrixPanel_I2S_DMA::color565(255, 140, 0);
+    default:
+        return MatrixPanel_I2S_DMA::color565(0, 0, 0);
+    }
+}
+
+void Display::printText(String text, int16_t x, int16_t y, Color color)
+{
+    dma_display->setTextSize(1); // size 1 == 8 pixels high
+    dma_display->setTextColor(this->color565(color));
+    dma_display->setCursor(x, y);
+    dma_display->print(text);
+}
+
+void Display::demo()
+{
+    dma_display->clearScreen();
+    dma_display->fillScreen(color565(BLACK));
+    dma_display->setTextWrap(false);
+
+    dma_display->setTextSize(1); // size 1 == 8 pixels high
+    dma_display->setTextColor(color565(DARK_ORANGE));
+    dma_display->setCursor(0, 0);
+    dma_display->print("21:00 Stockholm C");
+
+    dma_display->setCursor(0, 8);
+    dma_display->print("21:06 Falun");
+
+    dma_display->setCursor(0, 16);
+    dma_display->print("21:05 Eskilstuna");
+
+    dma_display->setCursor(0, 24);
+    dma_display->print("22:03 Uppsala");
 }
 
 // Implement your methods here
--- lib/display/src/display.h
+++ lib/display/src/display.h
@@ -1,13 +1,37 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+
 class Display
 {
 public:
     Display();
     ~Display();
 
-    // Add your methods here
+    enum Color
+    {
+        BLACK,
+        WHITE,
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        DARK_ORANGE
+    };
+
+    void demo();
+    void printText(String text, int16_t x, int16_t y, Color color = Color::DARK_ORANGE);
+    void clearScreen();
+
+    uint16_t color565(Color color);
+
+private:
+    // Add your variables here
+    MatrixPanel_I2S_DMA *dma_display = nullptr;
+    const uint16_t matrixWidth = 64;
+    const uint16_t matrixHeight = 32;
+    const uint16_t matrixChainLength = 2;
 };
 
 #endif // DISPLAY_H
