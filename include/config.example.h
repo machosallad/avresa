f51c191e68ef9a25e8a4f7ff80a8c9c908162c58
Jesper Melin <jesper.melin87@gmail.com>
2024-09-25 20:41:38 +0200

    Refactor configuration file
    Include additional parameters.

--- include/config.example.h
+++ include/config.example.h
@@ -1,10 +1,22 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <Arduino.h>
+
 // Define your sensitive settings here
 const char *WIFI_SSID = "your_wifi_ssid";
 const char *WIFI_PASSWORD = "your_wifi_password";
 const char *SERVER_KEY = "your_server_key";
 const char *SERVER_URL = "your_server_url";
+
+struct Config
+{
+    String ssid;
+    String password;
+    String serverKey;
+    String serverUrl;
+    String stationCode;
+    int brightness;
+};
 
 #endif // CONFIG_H
