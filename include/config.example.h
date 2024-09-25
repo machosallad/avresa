1374a4f5ce205065b7fd6f24b7c520cdd9b00557
Jesper Melin <jesper.melin87@gmail.com>
2024-09-25 21:35:37 +0200

    Refactor config.example.h
    Remove configuration which is no longer maintained in this file.

--- include/config.example.h
+++ include/config.example.h
@@ -7,16 +7,7 @@
 const char *WIFI_SSID = "your_wifi_ssid";
 const char *WIFI_PASSWORD = "your_wifi_password";
 const char *SERVER_KEY = "your_server_key";
-const char *SERVER_URL = "your_server_url";
 
-struct Config
-{
-    String ssid;
-    String password;
-    String serverKey;
-    String serverUrl;
-    String stationCode;
-    int brightness;
-};
+const char *STATION_CODE = "your_station_code";
 
 #endif // CONFIG_H
